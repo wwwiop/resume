a6ace68eac4537540b339fecf4791251d724bcdb
wwwiop <daniel@dirksen.com>
2022-01-12 16:31:29 -0800

    Now displays multi-column when screen larger than 45rem

--- index.js
+++ index.js
@@ -40,38 +40,51 @@ fetch(`resumes/${settings.name}.json`)
     resumeData.sections = resumeData.sections.filter(section => section.items.length > 0);
   }
 
-  document.title = `${resumeData.name} - Resume`;
+  document.title = `${resumeData.name}'s Resume`;
 
   let phoneNumberPure = resumeData.phoneNumber.split("").filter(digit => digit.search(/\(|\)|\s|-/) == -1).join("");
+  resumeData.sections.forEach(section => {
+    section.twoColumns = section.items.length > 1
+  })
 
   let resumeHTML = `
   <h1>${resumeData.name}</h1>
-  <h2>${resumeData.address}<br>
-  <a target="_blank" href="tel:${phoneNumberPure}">${resumeData.phoneNumber}</a> | <a target="_blank" href="mailto:${resumeData.emailAddress}">${resumeData.emailAddress}</a></h2>
+  <h2>${resumeData.address}<br></h2>
+  <h2>  
+    <a target="_blank" href="tel:${phoneNumberPure}">${resumeData.phoneNumber}</a>
+    <span class="divider-vert"></span>
+    <a target="_blank" href="mailto:${resumeData.emailAddress}">${resumeData.emailAddress}</a>
+    <span class="divider-vert"></span>
+    <a target="_blank" href="https://www.github.com/${resumeData.githubUsername}">github.com/${resumeData.githubUsername}</a>
+  </h2>
   ${
     resumeData.sections.map(section => `
-      <h3>${section.title}</h3>
-      <div class="divider"></div>
-      ${
-        section.items.map(item => `
-          <div class="item-container">
-            <h4>${item.title}</h4>
-            ${item.subtitle || item.dateRange ? `
-              <div class="subtitle">
-                ${item.subtitle ? `<span>${item.subtitle}</span>` : ``}
-                ${item.dateRange ? `<span>${item.dateRange}</span>` : ``}
-              </div>
-            ` : ``}
-            <ul>
-              ${
-                item.points.map(point => `
-                  <li>${point}</li>
-                `).join("")
-              }
-            </ul>
-          </div>
-        `).join("")
-      }
+      <div class="section">
+        <h3 id="${section.title}">${section.title}</h3>
+        <div class="divider"></div>
+        <div class="${section.twoColumns ? "two-columns" : ""}">
+        ${
+          section.items.map(item => `
+            <div class="item-container">
+              <h4>${item.title}</h4>
+              ${item.subtitle || item.dateRange ? `
+                <div class="subtitle">
+                  ${item.subtitle ? `<span>${item.subtitle}</span>` : ``}
+                  ${item.dateRange ? `<span>${item.dateRange}</span>` : ``}
+                </div>
+              ` : ``}
+              <ul>
+                ${
+                  item.points.map(point => `
+                    <li>${point}</li>
+                  `).join("")
+                }
+              </ul>
+            </div>
+          `).join("")
+        }
+        </div>
+      </div>
     `).join("")
   }
   `;
