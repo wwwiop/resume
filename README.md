1f5e13e43cf3ce8603c84bc11df8acafbb435741
wwwiop <daniel@dirksen.com>
2022-02-07 18:08:41 -0800

    Added presets

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 I created this firstly as a test, and secondly as a convienient way to access and share my resume in mobile- and printer-firnedly HTML format via [wwwiop.github.io/resume](https://wwwiop.github.io/resume). Content of the resume can be curated with a query string.
 
-Variations of my resume, useful in various times in my life:
+### Presets
+
+Flavors of my resume, which would have come in handdy at various times in the past:
+
 * [Default Resume](https://wwwiop.github.io/resume) - short URL with no query string.
-* [Artistic Resume](https://wwwiop.github.io/resume?i=bu-is-ss-sl-at-tm) - handy for small jobs and volunteer projects.
-* [CG Resume](https://wwwiop.github.io/resume/?i=bu-ni-si-ss-sl-tc-cg)
-* [Software Resume](https://wwwiop.github.io/resume?i=bu-ni-si-ss-sl-tc-tm) - unfortunately leaves out my experience flipping burgers, as priceless as it may be to the tech industry.
-* [1-Page Software Resume](https://wwwiop.github.io/resume?i=bu-ni-si-ss-sl) - for when brevity really counts.
+* [Artistic Resume](https://wwwiop.github.io/resume?p=at) - handy for small jobs and volunteer projects.
+* [CG Resume](https://wwwiop.github.io/resume/?p=cg) - a quick look at my 3D graphics experience.
+* [Software Resume](https://wwwiop.github.io/resume?p=sw) - unfortunately leaves out my experience flipping burgers, as priceless as it may be to the tech industry.
+* [Single-Page Software Resume](https://wwwiop.github.io/resume?p=sp) - for when brevity really counts.
+* [Combo](https://wwwiop.github.io/resume?p=al) - everything, including food-service.
+
+### Query String Parameters
+
+The content of the resume is determined by a query string which can specify different users, sets of experiences, or presets.
+
+| Parameter | Default Value                   | Description                                                         | Example          |
+| --------- | ------------------------------- | ------------------------------------------------------------------- | ---------------- |
+| n         | "dan".                          | The name of resume data JSON file to use, minus the ".json" suffix. | n=dan            |
+| i         | As defined in user's json file. | List of items to include, by ID, dash-seperated.                    | i=bu-ni-si-ss-sl |
+| p         | As defined in user's json file. | Preset by which to set inclusions.                                  | p=sw             |
--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+// default resume json file name, found in "resumes" folder
 let defaultName = "dan";
 // defaults to this pdf if anything goes wrong loading the resume.
 let backupURL = `pdf/${defaultName}.pdf`;
 let onError = function(error) {
   console.log(error);
-  location.href = backupURL;
+  //location.href = backupURL;
 }
 
 let settings = {
   name: defaultName,
+  presetID: null,
   inclusions: null
 };
 
 if (document.location.search) {
+  // convert query string to an object with values interpreted as strings
   let queryString = document.location.search.split("?")[1];
   let queryStringEntries = queryString.split("&").map(entryString => entryString.split("="));
   let queryStringObj = Object.fromEntries(queryStringEntries);
 
-  if (queryStringObj.n) settings.name = queryStringObj.n;
-  if (queryStringObj.i) settings.inclusions = queryStringObj.i;
+  // set setttings to query sring argument. leave it unchanged if argument is not found in query string
+  settings.name = queryStringObj.n || settings.name;
+  settings.presetID = queryStringObj.p || settings.presetID;
+  settings.inclusions = queryStringObj.i || settings.inclusions;
 }
 
 let resumeData;
@@ -26,12 +31,27 @@ let resumeEl = document.getElementById("resume");
 
 resumeEl.innerHTML = "Loading Resume...";
 
+// fetch resume data from "resumes" folder
 fetch(`resumes/${settings.name}.json`)
 .then(response => response.json())
 .then(data => {
+  // give global access to resume data - helpful for debugging
   resumeData = data;
-
-  if (!settings.inclusions) settings.inclusions = resumeData.defaultInclusions;
+  
+  // set resume inclusions
+  if (!settings.inclusions) {
+    // default the preset ID if one is not provided in query string
+    if (!settings.presetID) settings.presetID = resumeData.defautPresetID;
+    // use preset to get inclusions
+    if (!settings.inclusions) {
+      let preset = resumeData.presets.find(preset => preset.id == settings.presetID);
+      if (!preset) {
+        preset = resumeData.presets.find(preset => preset.id == resumeData.defautPresetID);
+      }
+      settings.inclusions = preset.inclusions;
+    };
+  };
+  // convert inclusions from string to array of strings
   settings.inclusions = settings.inclusions.split("-");
 
   if (settings.inclusions.length > 0) {
@@ -63,8 +83,11 @@ fetch(`resumes/${settings.name}.json`)
         <div class="divider"></div>
         <div class="${section.twoColumns ? "two-columns" : ""}">
         ${
-          section.items.map(item => `
-            <div class="item-container">
+          section.items.map((item, index) => `
+            <div class="item-container" style="${
+              // set style to span two columns if it is the only element in its row
+              (section.items.length % 2 == 1 && index == 0) ? `grid-column: 1 / 3` : ``
+            }">
               <h4>${item.title}</h4>
               ${item.subtitle || item.dateRange ? `
                 <div class="subtitle">
